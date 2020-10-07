91005ac80f01da9153bb8e30225c333c00d3c6e2
Daniel Duan <daniel@duan.ca>
2020-10-06 21:24:07 -0700

    Fix options
    They shouldn't be 32-bit ints.

--- Sources/Scmark/Options.swift
+++ Sources/Scmark/Options.swift
@@ -8,27 +8,27 @@ public struct Options: OptionSet {
     // MARK: - Options affecting rendering
 
     /// Include a `data-sourcepos` attribute on all block elements.
-    public static let sourcepos = 1 << 1
+    public static let sourcepos = Options(rawValue: 1 << 1)
 
     /// Render `softbreak` elements as hard line breaks.
-    public static let hardbreaks = 1 << 2
+    public static let hardbreaks = Options(rawValue: 1 << 2)
 
     /// Render raw HTML and unsafe links (`javascript:`, `vbscript:`,
     /// `file:`, and `data:`, except for `image/png`, `image/gif`,
     /// `image/jpeg`, or `image/webp` mime types).  By default,
     /// raw HTML is replaced by a placeholder HTML comment. Unsafe
     /// links are replaced by empty strings.
-    public static let unsafe = 1 << 17
+    public static let unsafe = Options(rawValue: 1 << 17)
 
     /// Render `softbreak` elements as spaces.
-    public static let noBreaks = 1 << 4
+    public static let noBreaks = Options(rawValue: 1 << 4)
 
     // MARK: - Options affecting parsing
 
     /// Validate UTF-8 in the input before parsing, replacing illegal
     /// sequences with the replacement character U+FFFD.
-    public static let validateUTF8 = 1 << 9
+    public static let validateUTF8 = Options(rawValue: 1 << 9)
 
     /// Convert straight quotes to curly, --- to em dashes, -- to en dashes.
-    public static let smart = 1 << 10
+    public static let smart = Options(rawValue: 1 << 10)
 }
